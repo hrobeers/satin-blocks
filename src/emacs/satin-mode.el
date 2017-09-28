d9f52ca53b81d566a4ca6d7ed0a9f133efa66f24
hrobeers <hansrobeers@gmail.com>
2017-09-28 23:00:14 +0200

    emacs: syntax highlight OP_ prefix optional

--- src/emacs/satin-mode.el
+++ src/emacs/satin-mode.el
@@ -23,34 +23,34 @@
 ;;; Code:
 
 ;; define several category of keywords
-(setq satin-keywords '("OP_NOP" "OP_IF" "OP_NOTIF" "OP_ELSE" "OP_ENDIF"
-                       "OP_VERIFY" "OP_EQUAL" "OP_EQUALVERIFY"
-                       "OP_RETURN") )
-(setq satin-types '("OP_RIPEMD160" "OP_SHA1" "OP_SHA256" "OP_HASH160" "OP_HASH256"
-                    "OP_CODESEPARATOR"
-                    "OP_CHECKSIG" "OP_CHECKSIGVERIFY" "OP_CHECKMULTISIG" "OP_CHECKMULTISIGVERIFY"))
-(setq satin-constants '("OP_0" "OP_1" "OP_2" "OP_3" "OP_4" "OP_5" "OP_6" "OP_7" "OP_8"
-                        "OP_9" "OP_10" "OP_11" "OP_12" "OP_13" "OP_14" "OP_15" "OP_16"
-                        "OP_TRUE" "OP_FALSE"
-                        "OP_PUSHDATA1" "OP_PUSHDATA2" "OP_PUSHDATA4"
-                        "OP_1NEGATE"))
-(setq satin-events '("OP_CHECKLOCKTIMEVERIFY" "OP_CHECKSEQUENCEVERIFY"))
-(setq satin-functions '("OP_TOALTSTACK" "OP_FROMALTSTACK" "OP_IFDUP" "OP_DEPTH"
-                        "OP_DROP" "OP_DUP" "OP_NIP" "OP_OVER" "OP_PICK"
-                        "OP_ROLL" "OP_ROT" "OP_SWAP" "OP_TUCK"
-                        "OP_2DROP" "OP_2DUP" "OP_3DUP" "OP_2OVER" "OP_2ROT" "OP_2SWAP"
-                        "OP_SIZE" "OP_1ADD" "OP_1SUB" "OP_NEGATE" "OP_ABS" "OP_NOT" "OP_0NOTEQUAL"
-                        "OP_ADD" "OP_SUB" "OP_BOOLAND" "OP_BOOLOR"
-                        "OP_NUMEQUAL" "OP_NUMEQUALVERIFY" "OP_NUMNOTEQUAL"
-                        "OP_LESSTHAN" "OP_GREATERTHAN" "OP_LESSTHANOREQUAL" "OP_GREATERTHANOREQUAL"
-                        "OP_MIN" "OP_MAX" "OP_WITHIN"))
+(setq satin-keywords '("NOP" "IF" "NOTIF" "ELSE" "ENDIF"
+                       "VERIFY" "EQUAL" "EQUALVERIFY"
+                       "RETURN") )
+(setq satin-types '("RIPEMD160" "SHA1" "SHA256" "HASH160" "HASH256"
+                    "CODESEPARATOR"
+                    "CHECKSIG" "CHECKSIGVERIFY" "CHECKMULTISIG" "CHECKMULTISIGVERIFY"))
+(setq satin-constants '("0" "1" "2" "3" "4" "5" "6" "7" "8"
+                        "9" "10" "11" "12" "13" "14" "15" "16"
+                        "TRUE" "FALSE"
+                        "PUSHDATA1" "PUSHDATA2" "PUSHDATA4"
+                        "1NEGATE"))
+(setq satin-events '("CHECKLOCKTIMEVERIFY" "CHECKSEQUENCEVERIFY"))
+(setq satin-functions '("TOALTSTACK" "FROMALTSTACK" "IFDUP" "DEPTH"
+                        "DROP" "DUP" "NIP" "OVER" "PICK"
+                        "ROLL" "ROT" "SWAP" "TUCK"
+                        "2DROP" "2DUP" "3DUP" "2OVER" "2ROT" "2SWAP"
+                        "SIZE" "1ADD" "1SUB" "NEGATE" "ABS" "NOT" "0NOTEQUAL"
+                        "ADD" "SUB" "BOOLAND" "BOOLOR"
+                        "NUMEQUAL" "NUMEQUALVERIFY" "NUMNOTEQUAL"
+                        "LESSTHAN" "GREATERTHAN" "LESSTHANOREQUAL" "GREATERTHANOREQUAL"
+                        "MIN" "MAX" "WITHIN"))
 
 ;; generate regex string for each category of keywords
-(setq satin-keywords-regexp (regexp-opt satin-keywords 'words))
-(setq satin-type-regexp (regexp-opt satin-types 'words))
-(setq satin-constant-regexp (regexp-opt satin-constants 'words))
-(setq satin-event-regexp (regexp-opt satin-events 'words))
-(setq satin-functions-regexp (regexp-opt satin-functions 'words))
+(setq satin-keywords-regexp (concat "\\(OP_\\)?" (regexp-opt satin-keywords 'words)))
+(setq satin-type-regexp (concat "\\(OP_\\)?" (regexp-opt satin-types 'words)))
+(setq satin-constant-regexp (concat "\\(OP_\\)?" (regexp-opt satin-constants 'words)))
+(setq satin-event-regexp (concat "\\(OP_\\)?" (regexp-opt satin-events 'words)))
+(setq satin-functions-regexp (concat "\\(OP_\\)?" (regexp-opt satin-functions 'words)))
 
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
